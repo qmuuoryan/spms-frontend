4af31a7631143e618c769b660e5b18e4906cb51a
qmuuoryan <ryanmuuo91@gmail.com>
2025-06-16 23:48:57 +0300

    home page import

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
+import 'screens/student_dashboard.dart';
+import 'screens/supervisor_dashboard.dart';
+import 'index_page.dart';
 
 void main() {
-  runApp(SPMSApp());
+  runApp(const SPMSApp());
 }
 
 class SPMSApp extends StatelessWidget {
+  const SPMSApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SPMS',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.teal),
-      home: LoginScreen(),
+      home: const IndexPage(),
     );
   }
 }
