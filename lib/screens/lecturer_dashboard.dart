096f9cfff0a863e27fd4b830d8478106efe638dc
qmuuoryan <ryanmuuo91@gmail.com>
2025-06-17 23:49:09 +0300

    lecturer dashboard is working

---
+++ lib/screens/lecturer_dashboard.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LecturerDashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Lecturer Dashboard")),
+      body: Center(child: Text("Welcome Lecturer!")),
+    );
+  }
+}
