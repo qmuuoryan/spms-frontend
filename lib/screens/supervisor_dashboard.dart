5ddf4b8d23b7623ad169d8c426eedc9bf318c4de
qmuuoryan <ryanmuuo91@gmail.com>
2025-06-17 23:51:33 +0300

    supervisor dashboard working

---
+++ lib/screens/supervisor_dashboard.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SupervisorDashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Supervisor Dashboard")),
+      body: Center(child: Text("Welcome Supervisor!")),
+    );
+  }
+}
