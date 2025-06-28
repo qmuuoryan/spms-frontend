9b3fe8da978e96e772d1c7252d0490616f4fb206
qmuuoryan <ryanmuuo91@gmail.com>
2025-06-28 21:40:45 +0300

    frontend footer for the UI

---
+++ lib/widgets/footer.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Footer extends StatelessWidget {
+  const Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Divider(thickness: 1, color: Colors.teal),
+        SizedBox(height: 8),
+        Text("© 2025 SPMS. All rights reserved.", style: TextStyle(fontSize: 14)),
+        SizedBox(height: 4),
+        Text("Developed by https://github.com/qmuuoryan", style: TextStyle(fontSize: 12)),
+        SizedBox(height: 16),
+      ],
+    );
+  }
+}
